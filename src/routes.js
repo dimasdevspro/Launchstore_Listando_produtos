695c066c73a54459e900355715435cf7720c5589
dimasdevspro <dimasdevspro@github.com>
2021-01-22 16:44:33 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const routes = express.Router()
+const multer = require('./app/middlewares/multer')
+const ProductController = require('./app/controllers/ProductController')
+const HomeController = require('./app/controllers/HomeController')
+const SearchController = require('./app/controllers/SearchController')
+
+//Home
+routes.get("/", HomeController.index);
+//Views
+routes.get("/views/about", function(req, res){
+    return res.render("about.njk")
+})
+
+// Search
+routes.get('/products/search', SearchController.index)
+
+//Admin
+routes.get("/products/create", ProductController.create);
+routes.get("/products/:id", ProductController.show)
+routes.get("/products/:id/edit", ProductController.edit)
+routes.post('/products', multer.array("photos", 6),ProductController.post)
+routes.put('/products', multer.array("photos", 6), ProductController.put)
+routes.delete('/products', ProductController.delete)
+
+// Alias
+routes.get("/ads/create", function(req, res){
+    return res.redirect("/products/create")
+});
+
+module.exports = routes
